f97070094cce6b82ecf2853723e69b80dde6844b
t-k- <clock126@126.com>
2015-11-14 15:50:42 -0500

    spacial case for email input

--- test/gen-form.sh
+++ test/gen-form.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+function echo_input()
+{
+	echo "$name: "
+	echo "<input type=\"$1\" name=\"$2\" id=\"$2\" value=\"$3\"><br/>"
+}
+
 echo '<html>'
 echo '<body>'
 while read l
 do
 	name=`echo $l | awk '{print $1}'`
 	value=`echo $l | awk '{$1=""; print substr($0,2);}'`
-	echo "$name: "
-	echo "<input type=\"text\" name=\"$name\" id=\"$name\" value=\"$value\"><br/>"
+	echo_input "text" $name $value;
+	if [ "$name" == "email" ]; then
+		echo_input "email" $name $value;
+	fi;
 done < gen-form.list
 echo '</body>'
 echo '</html>'
